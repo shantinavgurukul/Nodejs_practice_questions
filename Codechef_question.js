8778cb5a68007c9bf80d5e9b2364324769f86e5c
It's Shanti <you@example.com>
2021-01-30 18:56:52 +0530

    first commit

---
+++ Codechef_question.js
@@ -0,0 +1,92 @@
+// Odd/even_number
+// for(var i=1;i<10;i++){
+//     if(i%2==0){
+//         console.log(i,"even number")
+//     }
+//     else{
+//         console.log(i,"odd number")
+//     }
+// }
+
+//reverse order:-
+//  for(i = 20; i>=1;i--){
+//      console.log(i)
+//  }
+
+// product of number:-
+// let product=1;
+// for(var i =1;i<=10;i++){
+//     product = product*i;
+// }
+// console.log(product)
+
+// 10 even number:-
+// for(let i=2;i<=20;i+=2){
+//     console.log(i)
+
+// }
+
+// 14 odd number:-
+// let i=3;
+// while(i<30){
+//     if(i%2!=0)
+//     console.log(i);
+//     i++
+// }
+
+// print hello word 5 times:-
+// var readlineSync = require("readline-sync");
+// var num = readlineSync.question("enter the number=");
+// let i =1;
+// while(i<=num){
+//     console.log("hello")
+//     i++;
+
+// }
+
+// Tables
+// var tables = require("readline-sync")
+// var num = tables.question("enter the number=")
+// let i=1;
+// while(i<=10){
+//     console.log(num+"*"+i+"="+num*i)
+//     i++
+// }
+// first number raised to the power of another.
+// var pow = require("readline-sync")
+// var num = pow.question("enter the number=")
+// var n = pow.question("enter the number=")
+// let i=0;
+// while(i<1){
+//     console.log(num**n)
+//     i++
+// }
+// var number = require("readline-sync")
+// var num = number.question("enter the number=")
+// let i =1
+// while(i<=num){
+//     var n = number.question("enter the number=")
+//     if(n%2==0){
+//         console.log("even")
+//     }
+//     else{
+//         console.log("odd")
+//     }
+//     i++
+// }
+
+
+// patternQuestions
+// i=1
+// while(i<=25){
+//     j=i+4
+//     m=""
+//     while(j>=i){
+//         m=m+j
+//         j--
+//     }   
+//     console.log(m)
+//     i+=5
+// }
+
+
